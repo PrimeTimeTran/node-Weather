33cca243d82538f0582fdc66c35d4497be6aaa8d
Loi Tran <datloiboi@gmail.com>
2020-01-07 16:33:45 +0700

    Cleanup prompt

--- public/js/app.js
+++ public/js/app.js
@@ -4,13 +4,14 @@ const messageTwo = document.querySelector("#message-2");
 
 weatherForm.addEventListener("submit", async e => {
   e.preventDefault();
-  const search = document.querySelector("input").value;
+  const search = document.querySelector("input");
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
 
   const response = await fetch(
-    `/weather?address=${search}`
+    `/weather?address=${search.value}`
   );
+  search.value = ''
   const data = await response.json();
   if (data) {
     messageOne.textContent = data.location;
